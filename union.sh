ed1a42f66b82a571359d5b95c6b18a544c2da695
Thomas Levine <occurrence@thomaslevine.com>
2013-03-29 11:39:26 -0400

    union databases

---
+++ union.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Union all of the sqlite databases.
+set -e
+
+db=/tmp/logs.db
+rm -f "${db}"
+
+union() {
+  sqlite3 "${db}" "
+  ATTACH DATABASE '$1' AS new;
+  INSERT INTO main.log_msg SELECT * FROM new.log_msg;
+  INSERT INTO main.log_status SELECT * FROM new.log_status;
+  DETACH DATABASE new;
+  "
+}
+
+# Apply the schema
+sqlite3 logs/2012-09-15.db .schema | sqlite3 "${db}"
+
+# Add the new data.
+for new in $(ls logs); do
+  union logs/"${new}" || echo Error in "${new}"
+done
