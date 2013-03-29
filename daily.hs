ca0cf4f73da8e317b976dd54038dbda50df9d6be
Thomas Levine <occurrence@thomaslevine.com>
2013-03-29 13:02:53 -0400

    wrap sql query stuff

--- daily.hs
+++ daily.hs
@@ -14,14 +14,25 @@ users conn = M.fromList $
     query = quickQuery conn "SELECT uid, nick FROM log_status GROUP BY uid;" []
 -}
 
-getUsers :: Connection -> IO [[String]]
-getUsers conn = do 
-  query <- quickQuery conn "SELECT uid, nick FROM log_status GROUP BY uid;" []
+getQuery :: String -> String -> Connection -> IO [[String]]
+getQuery sql values conn = do
+  query <- quickQuery conn sql $ map toSql values
   return $ unSql query
   where
     unSql :: [[SqlValue]] -> [[String]]
     unSql = map $ map (\sql -> fromSql sql :: String)
 
+getUsers :: Connection -> IO [[String]]
+getUsers = getQuery "SELECT uid, nick FROM log_status GROUP BY uid;" []
+
+{-
+getUserStatus :: Connection -> IO [[String]]
+getUsers conn uid = do 
+  query <- quickQuery conn "SELECT * FROM log_status WHERE uid = ?" [uid]
+  return $ unSql query
+-}
+
+-- All features are maps from usernames to feature values.
 usernames :: [[String]] -> M.Map String String
 usernames query = M.fromList $ map (\l -> (head l, last l)) query
 
