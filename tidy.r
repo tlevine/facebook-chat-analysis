30ae64d324b2ea76b3a87e26b8bfb541bc3cd7c5
Thomas Levine <occurrence@thomaslevine.com>
2013-06-05 15:30:17 -0400

    count duration within an interval

--- tidy.r
+++ tidy.r
@@ -10,31 +10,30 @@ load <- function() {
 # data.frame -> data.frame
 set.types <- function(df) {
   df$date <- as.POSIXct(df$ts, origin = '1970-01-01')
-  df$ts <- NULL
   df$uid <- as.factor(df$uid)
   df$nick <- as.factor(df$nick)
   df$status <- factor(df$status, levels = c('avail', 'notavail'))
   df
 }
 
-# Total duration online based on the date column
+# Total duration online based on the ts column
 # If you want to group by uid or date, do that first.
-# data.frame -> data.frame
-duration <- function(df, start.date, end.date) {
+# data.frame -> num (seconds)
+duration <- function(df, start.ts, end.ts) {
   # Skip empty data frames.
   if (nrow(df) == 0) {
     return(0)
   }
 
-  # Deal with sessions spanning date cuts
+  # Deal with sessions spanning ts cuts
   if (df$status[1] == 'notavail') {
-    head.duration <- df$date[1] - start.date
+    head.duration <- df$ts[1] - start.ts
     df <- df[-1,]
   } else {
     head.duration <- 0
   }
   if (df$status[nrow(df)] == 'avail') {
-    tail.duration <- end.date - df$date[nrow(df)]
+    tail.duration <- end.ts - df$ts[nrow(df)]
     df <- df[-nrow(df),]
   } else {
     tail.duration <- 0
@@ -42,7 +41,8 @@ duration <- function(df, start.date, end.date) {
 
   # Now we can assume that the data frame starts with 'avail', ends with 'notavail' and alternates.
   avail <- ((1:nrow(df)) %% 2) == 1
-  cumsum(df[-avail,'date'] - df[avail,'date']) + head.duration + tail.duration
+  notavail <- ((1:nrow(df)) %% 2) == 0
+  sum(df[notavail,'ts'] - df[avail,'ts']) + head.duration + tail.duration
 }
 
 # IO ()
