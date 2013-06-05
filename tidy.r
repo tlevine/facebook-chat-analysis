297184381c5f95959ce5094b94e3138272022b04
Thomas Levine <occurrence@thomaslevine.com>
2013-06-05 14:56:12 -0400

    posixct

--- tidy.r
+++ tidy.r
@@ -1,14 +1,25 @@
 #!/usr/bin/env Rscript
 library(RSQLite)
 
-# IO DataFrame
+# IO data.frame
 load <- function() {
   connection = dbConnect('SQLite', dbname = '/tmp/logs.db')
-  dbReadTable(connection, name = 'log_status')
+  dbGetQuery(connection, 'SELECT "uid", "nick", "ts", "status" FROM log_status;')
+}
+
+# data.frame -> data.frame
+set.types <- function(df) {
+  df$ts <- as.POSIXct(df$ts, origin = '1970-01-01')
+  df$uid <- as.factor(df$uid)
+  df$nick <- as.factor(df$nick)
+  df$status <- factor(df$status, levels = c('avail', 'notavail'))
+  df
 }
 
 # IO ()
 main <- function () {
-  df = load()
+  df <- load()
+  df <- set.types(df)
+  df
 }
 
